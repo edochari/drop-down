526aea00f9f3d10f75e0b6130556c2dd04c86199
edochari <120657291+edochari@users.noreply.github.com>
2023-03-02 11:22:51 +0530

    final commit

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -1,20 +1,36 @@
 import { useState } from "react"
-
+import { FaAngleDown } from "react-icons/fa";
 export const DropDown=(props)=>{
-    const [style,setStyle]=useState("options");
-
-    const handleClick=()=>{
-        setStyle("options-clicked");
+    
+    const [showMenu,setShowMenu]=useState(false);
+    const [selectedValue,setSelectedValue]=useState(null);
+   
+    const getDisplay=()=>{
+            if(selectedValue){
+                return selectedValue;
+            }
+            return `select ... `;
     }
+    const handleClick=(item)=>{
+        setSelectedValue(item);
+        setShowMenu(false);
+        console.log("selected",item);
+    }
+
     return (
         <div className='drop-down-container'>
             <label id="text">Should you use a drop-down?</label>
             <div className="dropdown">
-                <button className="button">Select</button>
-                <div className={style}>
-                    {props.items.map(item=>
-                    <span className="item" onClick={handleClick}>{item}</span>)}
+                <div className="dropdown-box">
+                    <button className="button"  onMouseEnter={() => setShowMenu(true)}>{getDisplay()}</button>
+                    <FaAngleDown style={{ marginTop: 6,marginBottom:0}}/>
                 </div>
+                {showMenu && 
+                <div className="options">
+                    {props.items.map(item=>
+                    <span className="item" onClick={()=>handleClick(item)}>{item}</span>)}
+                </div>
+                }
             </div>   
             
         </div>
